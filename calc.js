45e6b8fe0f99c6b3797b8c21e7a437db9249cfa3
Soichiro Kato <m17se004@m.nanzan-u.ac.jp>
2018-06-11 16:22:40 +0900

    renewed the algorithm of calculating working hours

--- calc.js
+++ calc.js
@@ -5,44 +5,81 @@ $(() => {
 
 function calc()
 {
-  var sHour = $('#started').find('input[type="number"]').eq(0).val();
-  var sMin  = $('#started').find('input[type="number"]').eq(1).val();
-  var eHour = $('#ended').find('input[type="number"]').eq(0).val();
-  var eMin  = $('#ended').find('input[type="number"]').eq(1).val();
-  var bHour = $('#break').find('input[type="number"]').eq(0).val();
-  var bMin  = $('#break').find('input[type="number"]').eq(1).val();
+  var worked_time = 0;
+  var break_time  = 0;
+  var total_time  = 0;
 
-  var started = new Date();
-  var ended   = new Date();
-  started.setHours(sHour, sMin, 0, 0);
-  ended.setHours(eHour, eMin, 0, 0);
+  /* calclate total time of worked time */
+  $('#working_hours_list tr').each(function() {
+    if($(this).find('th').length) {
+      return;
+    }
 
-  if(ended.getTime() < started.getTime()) {
-    alert('ended time is earlier than started time!!');
-    $('#result').text('error');
+    var sHour = $(this).find('.hour').eq(0).val();
+    var sMin  = $(this).find('.min').eq(0).val();
+    var eHour = $(this).find('.hour').eq(1).val();
+    var eMin  = $(this).find('.min').eq(1).val();
+
+    var started = new Date();
+    var ended   = new Date();
+    started.setHours(sHour, sMin, 0, 0);
+    ended.setHours(eHour, eMin, 0, 0);
+
+    var diff =
+      parseInt((ended.getTime() - started.getTime()) / (60 * 1000));
+
+    if(diff < 0) {
+      worked_time = -1;
+    } else if(worked_time >= 0) {
+      worked_time += diff
+    }
+  });
+
+  if(worked_time < 0) {
+    var e_msg = 'The Ended time is earlier than started time!!';
+    // alert(e_msg);
+    $('#result').text('Error: ' + e_msg);
     return;
   }
-  var diff = (ended.getTime() - started.getTime()) / (60 * 1000);
-  var ans  = diff - (parseInt(bHour) * 60 + parseInt(bMin));
 
-  if(ans < 0) {
-    alert('break time is more than working time!!');
-    $('#result').text('error');
+  break_time = 
+    parseInt($('#break_time input.hour').val()) * 60
+    + parseInt($('#break_time input.min').val());
+
+  total_time = worked_time - break_time;
+  if(total_time < 0) {
+    var e_msg = 'Your break time is longer than working time!!';
+    // alert(e_msg);
+    $('#result').text('Error: ' + e_msg);
     return;
   }
 
-  $('#result').text(Math.floor(ans / 60) + 'H ' + ans % 60 + 'min');
+  var result_text = 'You worked '
+    + parseInt(total_time / 60) + 'H ' + total_time % 60 + 'min long'
+  $('#result').text(result_text);
 }
 
 function format(form)
 {
   var val = parseInt($(this).val());
-  if(!Number.isInteger(val)) {
-    $(this).val('00');
+  if(!Number.isInteger(val) || val < 0) {
+    switch ($(this).attr('class')) {
+      case 'hour':
+        $(this).val('0');
+        break;
+      case 'min':
+        $(this).val('00');
+        break;
+    }
   } else if(val < 10) {
-    $(this).val('0' + val);
-  } else if(val < 0) {
-    $(this).val('00');
+    switch ($(this).attr('class')) {
+      case 'hour':
+        $(this).val(val);
+        break;
+      case 'min':
+        $(this).val('0' + val);
+        break;
+    }
   }
 
   if($(this).attr('class') === 'hour') {
