3d177200a5ecf812646dfa68444a9c092d427b2d
Soichiro Kato <m17se004@m.nanzan-u.ac.jp>
2018-07-28 18:07:56 +0900

    changed framework from jQuery to Vue.js

--- calc.js
+++ calc.js
@@ -1,141 +1,53 @@
-$(function() {
-  $('button#calc').on('click', calc);
-  $('input[type="number"]').on('change blur', format);
+var app = new Vue({
+  el: '#app',
+  data: {
+    workedList: [
+      {
+        started: { hour: 10, min: 0 },
+        ended:   { hour: 15, min: 0 }
+      }
+    ],
+    breakTime: { hour: 0, min: 0 }
+  },
 
-  $(document).on('click', '.add_working_hours_row', function() {
-    var table      = $(this).parents('table');
-    var tr         = $('<tr>');
-    var addButton  = $('<button>');
-    var delButton  = $('<button>');
-    var sHourInput = $('<input>');
-    var sMinInput  = $('<input>');
-    var eHourInput = $('<input>');
-    var eMinInput  = $('<input>');
+  computed: {
+    workedTime: function() {
+      var total = 0;
+      for (var item of this.workedList) {
+        var started = new Date();
+        var ended   = new Date();
+        started.setHours(item.started.hour, item.started.min, 0, 0);
+        ended.setHours(item.ended.hour, item.ended.min, 0, 0);
 
-    addButton.attr({'type' : 'button', 'class' : 'add_working_hours_row'});
-    addButton.text('+');
-    delButton.attr({'type' : 'button', 'class' : 'del_working_hours_row'});
-    delButton.text('-');
+        var diff = parseInt((ended.getTime() - started.getTime()) / (60 * 1000));
+        if (diff < 0) {
+          return 'Error: 終了時刻が開始時刻より早いです';
+        }
+        total += diff;
+      }
 
-    sHourInput.attr({'type' : 'number', 'class': 'hour', 'max' : '23'});
-    sHourInput.val('10');
-    sMinInput.attr({'type' : 'number', 'class' : 'min', 'max' : '59'});
-    sMinInput.val('00');
-    eHourInput.attr({'type' : 'number', 'class' : 'hour', 'max' : '23'});
-    eHourInput.val('15');
-    eMinInput.attr({'type' : 'number', 'class' : 'min', 'max' : '59'});
-    eMinInput.val('00');
-
-    tr.append([
-      $('<td>').append(addButton),
-      $('<td>').append(delButton),
-      $('<td>').append(sHourInput)
-               .append(document.createTextNode(" 時 "))
-               .append(sMinInput)
-               .append(document.createTextNode(" 分 ")),
-      $('<td>').append(eHourInput)
-               .append(document.createTextNode(" 時 "))
-               .append(eMinInput)
-               .append(document.createTextNode(" 分 "))
-    ]);
-    table.append(tr);
-
-    $('button.del_working_hours_row').prop('disabled', false);
-  });
-
-  $(document).on('click', '.del_working_hours_row', function() {
-    $(this).parents('tr').remove();
-
-    if ($('button.del_working_hours_row').length == 1) {
-      $('button.del_working_hours_row').prop('disabled', true);
+      var restTime = parseInt(this.breakTime.hour) * 60
+                     + parseInt(this.breakTime.min)
+      total -= restTime
+      if (total < 0) {
+        return 'Error: 休憩時間が勤務時間より長いです';
+      } else {
+        return '勤務時間: ' + parseInt(total / 60) + '時'
+                            + parseInt(total % 60) + '分';
+      }
     }
-  });
+  },
+  
+  methods: {
+    addRow: function() {
+      var data = {
+        started: { hour: 10, min: 0 },
+        ended:   { hour: 15, min: 0 }
+      };
+      this.workedList.push(data);
+    },
+    delRow: function(index) {
+      this.workedList.splice(index, 1);
+    }
+  }
 });
-
-function calc()
-{
-  var worked_time = 0;
-  var break_time  = 0;
-  var total_time  = 0;
-
-  /* calclate total time of worked time */
-  $('#working_hours_list tr').each(function() {
-    if ($(this).find('th').length) {
-      return;
-    }
-
-    var sHour = $(this).find('.hour').eq(0).val();
-    var sMin  = $(this).find('.min').eq(0).val();
-    var eHour = $(this).find('.hour').eq(1).val();
-    var eMin  = $(this).find('.min').eq(1).val();
-
-    var started = new Date();
-    var ended   = new Date();
-    started.setHours(sHour, sMin, 0, 0);
-    ended.setHours(eHour, eMin, 0, 0);
-
-    var diff =
-      parseInt((ended.getTime() - started.getTime()) / (60 * 1000));
-
-    if (diff < 0) {
-      worked_time = -1;
-    } else if (worked_time >= 0) {
-      worked_time += diff
-    }
-  });
-
-  if (worked_time < 0) {
-    var e_msg = '終了時刻が開始時刻より早いです';
-    // alert(e_msg);
-    $('#result').text('Error: ' + e_msg);
-    return;
-  }
-
-  break_time = 
-    parseInt($('#break_time input.hour').val()) * 60
-    + parseInt($('#break_time input.min').val());
-
-  total_time = worked_time - break_time;
-  if (total_time < 0) {
-    var e_msg = '休憩時間が勤務時間より長いです';
-    // alert(e_msg);
-    $('#result').text('Error: ' + e_msg);
-    return;
-  }
-
-  var result_text = '勤務時間: '
-    + parseInt(total_time / 60) + '時間' + total_time % 60 + '分'
-  $('#result').text(result_text);
-}
-
-function format(form)
-{
-  var val = parseInt($(this).val());
-  if (!Number.isInteger(val) || val < 0) {
-    switch ($(this).attr('class')) {
-      case 'hour':
-        $(this).val('0');
-        break;
-      case 'min':
-        $(this).val('00');
-        break;
-    }
-  } else if (val < 10) {
-    switch ($(this).attr('class')) {
-      case 'hour':
-        $(this).val(val);
-        break;
-      case 'min':
-        $(this).val('0' + val);
-        break;
-    }
-  }
-
-  if ($(this).attr('class') === 'hour') {
-    if (val > 23) {
-      $(this).val('23');
-    }
-  } else if (val >= 60) {
-    $(this).val('59');
-  }
-}
