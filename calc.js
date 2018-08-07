1aff6be192f46b316d5f48bc5e2105029fa0f15e
Soichiro Kato <m17se004@m.nanzan-u.ac.jp>
2018-08-07 14:34:23 +0900

    added input form validation

--- calc.js
+++ calc.js
@@ -16,15 +16,32 @@ var app = new Vue({
       for (var item of this.workedList) {
         var started = new Date();
         var ended   = new Date();
+
+        // filtering input values of working time
+        item.started.hour =
+          this.checkMaxLimit(this.checkMinLimit(item.started.hour, 0), 23);
+        item.started.min =
+          this.checkMaxLimit(this.checkMinLimit(item.started.min, 0), 59);
+        item.ended.hour =
+          this.checkMaxLimit(this.checkMinLimit(item.ended.hour, 0), 23);
+        item.ended.min =
+          this.checkMaxLimit(this.checkMinLimit(item.ended.min, 0), 59);
+
         started.setHours(item.started.hour, item.started.min, 0, 0);
         ended.setHours(item.ended.hour, item.ended.min, 0, 0);
 
-        var diff = parseInt((ended.getTime() - started.getTime()) / (60 * 1000));
+        var diff =
+          parseInt((ended.getTime() - started.getTime()) / (60 * 1000));
         if (diff < 0) {
           return 'Error: 終了時刻が開始時刻より早いです';
         }
         total += diff;
       }
+
+      // filtering input values of break time
+      this.breakTime.hour = this.checkMinLimit(this.breakTime.hour, 0);
+      this.breakTime.min =
+        this.checkMaxLimit(this.checkMinLimit(this.breakTime.min, 0), 59);
 
       var restTime = parseInt(this.breakTime.hour) * 60
                      + parseInt(this.breakTime.min)
@@ -37,7 +54,7 @@ var app = new Vue({
       }
     }
   },
-  
+
   methods: {
     addRow: function() {
       var data = {
@@ -48,6 +65,22 @@ var app = new Vue({
     },
     delRow: function(index) {
       this.workedList.splice(index, 1);
+    },
+    checkMinLimit: function(val, min) {
+      if (typeof val !== 'number') {
+        return min;
+      } else if (val < min) {
+        return min;
+      }
+      return val;
+    },
+    checkMaxLimit: function(val, max) {
+      if (typeof val !== 'number') {
+        return max;
+      } else if (val > max) {
+        return max;
+      }
+      return val;
     }
   }
 });
